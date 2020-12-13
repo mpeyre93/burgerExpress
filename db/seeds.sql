eacb20c2bc2b9df7f2a837c47eebf50ab0addd59
Marlee Eyre <marlee.eyre@gmail.com>
2020-12-13 13:16:49 -0500

    burger database created, schema and seeds files added

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chipotle Ranch Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
