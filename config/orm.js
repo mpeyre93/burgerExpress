b4647432641a86d29a0059aa16ae19d85f9aecbe
Marlee Eyre <marlee.eyre@gmail.com>
2020-12-14 13:28:24 -0500

    methods created selectAll, insertOne and updateOne to retrieve and store data in db

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,38 @@
-var connection = require ("./connection.js");
+//import MySQL connection
+var connection = require ("../config/connection.js");
 
+var orm = {
+    selectAll: function(tableInput, cb){
+        var queryString = "SELECT * FROM" + tableInput + ";";
+        connection.query(queryString, function (err, result){
+            if (err){
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function(table, value, cb){
+        var queryString = "INSERT INTO" + table + "SET ?";
+        connection.query(queryString, value, function(err, result){
+            if (err){
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    updateOne: function(table, condition, id, cb){
+        var queryString =  "UPDATE" + table + "SET" + condition + "WHERE id = ?";
+        connection.query(queryString, id, function(err, result){
+            if (err){
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
 
-selectALL()
+module.exports = orm;
 
-
-insertOne()
-
-
-updateOne()
 
 
 module.exports = orm;
