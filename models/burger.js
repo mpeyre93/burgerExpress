a9d18c94f017220234f77aeeb29776d740c91614
Marlee Eyre <marlee.eyre@gmail.com>
2020-12-15 11:26:09 -0500

    select, create and update functions created to interact with db

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,24 @@
+//import ORM to create functions that will interact with DB
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cd){
+    all: function(cb){
         orm.all("burgers", function(res){
-            cd(res);
+            cb(res);
         });
     },
-    create: function(name, cd){
-        orm.create("burgers", [
-            "burger_name", "devoured"
-        ], [
-            name, false
-        ], cd);
+    create: function(name, cb){
+        orm.create("burgers", name, function(res){
+            cb(res);
+        });
     },
-    update: function(id, cd){
-        var condition = "id=" + id;
-        orm.update("burgers")
+    update: function(condition, id, cb){
+        condition = "devoured = true"
+        orm.update("burgers", condition, id, function(res){
+            cb(res);
+        });
     }
-}
+};
+
+//export database functions for burgers_controllers.js
+module.exports = burger;
